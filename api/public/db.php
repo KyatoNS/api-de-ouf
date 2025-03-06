dfa38307bdf60b20abdd336e9eec0caa2ff7d8bd
delmily <d3lmily@gmail.com>
2025-03-06 10:06:49 +0100

    ADD : migrationDatabase() in db.php

--- api/public/db.php
+++ api/public/db.php
@@ -17,3 +17,37 @@ function getDatabaseConnection(): PDO
 		"$databaseType:host=$servername;dbname=$dbname",
 		$username,
 		$password);
+
+
+function migrationDatabase(){
+	try {
+		$databaseConnection = getDatabaseConnection();
+		$databaseConnection->query("DROP TABLE IF EXISTS `user`, `order`;");
+		$databaseConnection->query("CREATE TABLE `user`(
+			`id_user` INT NOT NULL AUTO_INCREMENT,
+			`username` varchar(50) NOT NULL,
+			`password` varchar(50) NOT NULL,
+			`token` varchar(50),
+			PRIMARY KEY (`id_user`)
+			);
+			CREATE TABLE `order`(
+			`id_order` INT NOT NULL AUTO_INCREMENT,
+			`prix` INT NOT NULL,
+			`date` DATE NOT NULL,
+			PRIMARY KEY (`id_order`),
+			FOREIGN KEY (`id_user`) REFERENCES user(`id_user`)
+			);");
+
+		echo jsonResponse(201, [
+			"success" => true,
+			"message" => "Database created"
+		]);
+	} catch (Exception $exception) {
+		echo jsonResponse(500, [
+			"success" => false,
+			"error" => $exception->getMessage()
+		]);
+	}
+}
+	
+
