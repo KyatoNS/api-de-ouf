d67f9752ed29ef63fe3709d613bfd4cdbea716b6
delmily <d3lmily@gmail.com>
2025-03-07 14:33:43 +0100

    MODIFY : add verifToken to allOrders

--- api/public/routes/allOrders.php
+++ api/public/routes/allOrders.php
@@ -2,18 +2,30 @@
 
 require_once __DIR__ . "/../db.php";
 require_once __DIR__ . "/../Utils.php";
+require_once __DIR__ . "/../tokens.php";
 
 try {
+    $headers = getallheaders();
+    $head = ($headers['Authorization']);
+    $token = explode(" ", $head);
+    $token = $token[1];
+    if(verifyToken($token)){
+        $databaseConnection = getDatabaseConnection();
+        $getOrdersQuery = $databaseConnection->query("SELECT * FROM orders WHERE is_archived = FALSE;");
 
-    $databaseConnection = getDatabaseConnection();
-    $getOrdersQuery = $databaseConnection->query("SELECT * FROM orders WHERE is_archived = FALSE;");
+        $orders = $getOrdersQuery->fetchAll(PDO::FETCH_ASSOC);
 
-    $orders = $getOrdersQuery->fetchAll(PDO::FETCH_ASSOC);
+        echo jsonResponse(200, [
+            "success" => true,
+            "orders" => $orders
+        ]);
+    }else{
+        echo jsonResponse(401, [
+            "success" => false,
+            "message" => "Token not valid"
+        ]);
+    }
 
-    echo jsonResponse(200, [
-        "success" => true,
-        "orders" => $orders
-    ]);
 } catch (Exception $exception) {
     echo jsonResponse(500, [
         "success" => false,
