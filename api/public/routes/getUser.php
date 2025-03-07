7a1d0a6e74a4efc95a8ae7035aec0c9a370100d0
delmily <d3lmily@gmail.com>
2025-03-07 14:50:27 +0100

    MODIFY : verifToken in getUser.php

--- api/public/routes/getUser.php
+++ api/public/routes/getUser.php
@@ -2,33 +2,45 @@
 
 require_once __DIR__ . "/../db.php";
 require_once __DIR__ . "/../Utils.php";
+require_once __DIR__ . "/../tokens.php";
 
 try {
-    $path = $_SERVER["REQUEST_URI"];
-    $pathslited = explode("/", $path);
-    $parameter = end($pathslited);
+    $headers = getallheaders();
+    $head = ($headers['Authorization']);
+    $token = explode(" ", $head);
+    $token = $token[1];
+    if(verifyToken($token)){
+        $path = $_SERVER["REQUEST_URI"];
+        $pathslited = explode("/", $path);
+        $parameter = end($pathslited);
 
-    $databaseConnection = getDatabaseConnection();
-    $getUserQuery = $databaseConnection->prepare("SELECT * FROM users WHERE id_user=:id AND is_archived = FALSE;");
+        $databaseConnection = getDatabaseConnection();
+        $getUserQuery = $databaseConnection->prepare("SELECT * FROM users WHERE id_user=:id AND is_archived = FALSE;");
 
-    $getUserQuery->execute([
-        "id" => htmlspecialchars($parameter),
-    ]);
-
-    $user = $getUserQuery->fetchAll(PDO::FETCH_ASSOC);
-
-    if (empty($user)) {
-        echo jsonResponse(404, [
-            "success" => false,
-            "error" => "User not found!"
+        $getUserQuery->execute([
+            "id" => htmlspecialchars($parameter),
         ]);
-        die();
-    }
 
-    echo jsonResponse(200, [
-        "success" => true,
-        "users" => $user
-    ]);
+        $user = $getUserQuery->fetchAll(PDO::FETCH_ASSOC);
+
+        if (empty($user)) {
+            echo jsonResponse(404, [
+                "success" => false,
+                "error" => "User not found!"
+            ]);
+            die();
+        }
+
+        echo jsonResponse(200, [
+            "success" => true,
+            "users" => $user
+        ]);
+    }else{
+        echo jsonResponse(401, [
+            "success" => false,
+            "message" => "Token not valid"
+        ]);
+    }
 } catch (Exception $exception) {
     echo jsonResponse(500, [
         "success" => false,
