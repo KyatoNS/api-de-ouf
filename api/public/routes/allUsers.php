0cf163f2a842b6cab719dc8bf7f4591d10d0011f
delmily <d3lmily@gmail.com>
2025-03-07 14:29:08 +0100

    MODIFY : modify verifyToken & Add allusers token side

--- api/public/routes/allUsers.php
+++ api/public/routes/allUsers.php
@@ -2,18 +2,29 @@
 
 require_once __DIR__ . "/../db.php";
 require_once __DIR__ . "/../Utils.php";
+require_once __DIR__ . "/../tokens.php";
 
 try {
+    $headers = getallheaders();
+    $head = ($headers['Authorization']);
+    $token = explode(" ", $head);
+    $token = $token[1];
+    if(verifyToken($token)){
+        $databaseConnection = getDatabaseConnection();
+        $getUsersQuery = $databaseConnection->query("SELECT * FROM users WHERE is_archived = FALSE;");
 
-    $databaseConnection = getDatabaseConnection();
-    $getUsersQuery = $databaseConnection->query("SELECT * FROM users WHERE is_archived = FALSE;");
+        $users = $getUsersQuery->fetchAll(PDO::FETCH_ASSOC);
+        echo jsonResponse(200, [
+            "success" => true,
+            "users" => $users
+        ]);
+    }else{
+        echo jsonResponse(401, [
+            "success" => false,
+            "message" => "Token not valid"
+        ]);
+    }
 
-    $users = $getUsersQuery->fetchAll(PDO::FETCH_ASSOC);
-
-    echo jsonResponse(200, [
-        "success" => true,
-        "users" => $users
-    ]);
 } catch (Exception $exception) {
     echo jsonResponse(500, [
         "success" => false,
--- api/public/tokens.php
+++ api/public/tokens.php
@@ -8,33 +8,41 @@ function createToken($idUser){
     return $token;
 }
 
-function verifyExpirationToken($token){
-    $now = new DateTime();
+function verifyToken($token){
+    try{
+        $date = new DateTime();
+        $now = $date->getTimestamp();
     
-    $tokenDecode = base64_decode($token);
-    $tokensplit = explode(".", $tokenDecode);
-    echo($tokenDecode);
+        $tokenDecode = base64_decode($token);
+        $tokensplit = explode(".", $tokenDecode);
+
+        if (count($tokensplit) != 2|| is_int($tokensplit[0]) || is_int($tokensplit[1])){
+            return False;
+        }
+        
+        $idUser = $tokensplit[0];
+        $dateExpiration = $tokensplit[1];
+        
+        if ($dateExpiration > $now){
+            $databaseConnection = getDatabaseConnection();
+            $isOnDb = $databaseConnection->prepare("SELECT * FROM users WHERE token=:token AND is_archived = FALSE;");
     
-    $idUser = $tokensplit[0];
-    $dateExpiration = $tokensplit[1];
+            $isOnDb->execute([
+                "token" => $token,
+            ]);
+        
+            $isOnDbReponse = $isOnDb->fetchAll(PDO::FETCH_ASSOC);
+            
+            if (empty($isOnDbReponse)){
+                return False;
+            }else{
+                return True;
+            }
+        }else{
+            return False;
+        }
     
-    if ($dateExpiration < $now){
-        return True;
-    }else{
-        return False;
-    }
-
-    $isOnDb = $databaseConnection->prepare("SELECT * FROM users WHERE token=:token AND is_archived = FALSE;");
-
-    $isOnDb->execute([
-        "token" => htmlspecialchars($token),
-    ]);
-
-    $isOnDbReponse = $isOnDb->fetchAll(PDO::FETCH_ASSOC);
-
-    if (empty($isOnDbReponse)){
-        return True;
-    }else{
+    } catch (Exception $exception) {
         return False;
     }
 }
